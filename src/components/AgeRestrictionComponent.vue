bb5ecb127806dbb8dcc2df99179f3ff5741a79d2
Thomas Baer <thomas.baer@slub-dresden.de>
2021-03-18 15:11:08 +0100

    wip: action and restrictions

--- src/components/AgeRestrictionComponent.vue
+++ src/components/AgeRestrictionComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="">
+  <div>
     <label label-for="minage">Mindestalter</label>
     <b-form-input id="minage" v-model="restriction.minage" type="number"></b-form-input>
-    <label label-for="maxage">Mindestalter</label>
+    <label label-for="maxage">Höchstalter</label>
     <b-form-input id="maxage" v-model="restriction.maxage" type="number"></b-form-input>
   </div>
 </template>
--- src/components/RestrictionComponent.vue
+++ src/components/RestrictionComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="border-primary border-1px solid m-2 p-2 rounded">
+  <div class="border border-primary border-1px solid m-2 p-2 rounded">
     <h6>{{ restriction.type }}</h6>
-    <b-select v-model="restriction.type" :options="restrictionOptions"></b-select>
+    <b-select v-model="restriction.type" :options="restrictionOptions" v-on:change="resetRestriction()"></b-select>
     <DateRestrictionComponent v-if="restriction.type=='date'" :restriction="restriction"></DateRestrictionComponent>
     <AgeRestrictionComponent v-if="restriction.type=='age'" :restriction="restriction"></AgeRestrictionComponent>
   </div>
@@ -10,6 +10,8 @@
 <script>
 import DateRestrictionComponent from "@/components/DateRestrictionComponent";
 import AgeRestrictionComponent from "@/components/AgeRestrictionComponent";
+import {Restriction} from "@/librml/librml";
+
 export default {
   name: "RestrictionComponent",
   components: {DateRestrictionComponent, AgeRestrictionComponent},
@@ -22,6 +24,11 @@ export default {
         {value: 'date', text: "Zeitbeschränkung"},
         {value: 'age', text: "Altersbeschränkung"},
       ]
+    }
+  },
+  methods: {
+    resetRestriction() {
+      this.restriction = new Restriction();
     }
   }
 }
