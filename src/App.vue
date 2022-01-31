aef19e63b5470418e03fb6aa781bd4b6768e7741
Thomas Baer <thomas.baer@slub-dresden.de>
2022-01-31 13:40:09 +0100

    Produktiv auf Github.io Seite

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item class="nav-link" href="https://slub.github.io/librml/" target="_blank">
+              <b-nav-item class="nav-link" href="https://librml.org/index.html" target="_blank">
                 Über LibRML
               </b-nav-item>
               <b-nav-item v-b-modal.aboutinfo class="nav-link" href="#">
